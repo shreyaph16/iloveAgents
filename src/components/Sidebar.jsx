27ed2d5b16adbac7705ca73365221637274ff61d
Ridima <128479227+Ridima28@users.noreply.github.com>
2026-05-25 22:36:35 +0530

    Fix indentation in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,11 +31,11 @@ export default function Sidebar({ open, onClose }) {
         />
       )}
 
-<aside
-  className={`fixed top-14 left-0 bottom-0 z-40 w-60 flex flex-col border-r transition-all duration-200
-    dark:bg-surface dark:border-border bg-white border-gray-200
-    ${open ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0`}
->
+    <aside
+      className={`fixed top-14 left-0 bottom-0 z-40 w-60 flex flex-col border-r transition-all duration-200
+        dark:bg-surface dark:border-border bg-white border-gray-200
+        ${open ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0`}
+    >
       
         {/* Header */}
         <div className="px-4 py-3 flex items-center justify-between">
